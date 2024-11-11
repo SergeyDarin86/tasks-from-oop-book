9993a67085120f868b3afe81f2ceb5c4a621ef53
sergei_darin <swd86@mail.ru>
2024-11-11 21:57:54 +0400

    Рефактор кода

--- src/main/java/org/example/libraryArray/LibraryArray.java
+++ src/main/java/org/example/libraryArray/LibraryArray.java
@@ -4,67 +4,58 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class LibraryArray {
-        public static void main(String[] args) {
-            Scanner in = new Scanner(System.in);
-            int n;
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n;
 
-            do {
-                System.out.println("Введите натуральное число");
-                n = in.nextInt();
-            }while (Library.conditionVerify(n) == false);
-            in.close();
+        do {
+            System.out.println("Введите натуральное число");
+            n = in.nextInt();
+        } while (Library.conditionVerify(n) == false);
+        in.close();
 
-            final int MIN = 0;
-            final int MAX = 10;
+        final int MIN = 0;
+        final int MAX = 10;
 
-            int[] a = Library.generationArray(MIN, MAX, n);
-            System.out.println(Arrays.toString(a));
-            Library obj = new Library();
-            obj.maxIndex(a);
-            System.out.println("Минимальное значение: " + obj.min(a) + "\nИндекс последнего максимального значения: " + obj.getMaxInd());
+        int[] a = Library.generationArray(MIN, MAX, n);
+        System.out.println(Arrays.toString(a));
+        Library obj = new Library();
+        obj.maxIndex(a);
+        System.out.println("Минимальное значение: " + obj.min(a) + "\nИндекс последнего максимального значения: " + obj.getMaxInd());
+    }
+}
+
+class Library {
+
+    private int indMax = 0;
+
+    static boolean conditionVerify(int n) {
+        if (n <= 0) {
+            System.out.println("Число должно быть больше 0.\n" +
+                    "Попробуйте еще раз!\n");
+            return false;
+        }
+        return true;
+    }
+
+    static int[] generationArray(final int MIN, final int MAX, int n) {
+        int[] array = new int[n];
+        return Arrays.stream(array).map(value -> (int) (Math.random() * (MAX - MIN) + MIN)).toArray();
+    }
+
+    int min(int[] array) {
+        return Arrays.stream(array).min().getAsInt();
+    }
+
+    void maxIndex(int[] array) {
+        for (int i = 1; i < array.length; i++) {
+            if (array[indMax] <= array[i]) {
+                indMax = i;
+            }
         }
     }
 
-    class Library{
-
-        private int indMax = 0;
-
-        static boolean conditionVerify(int n){
-            if (n <= 0){
-                System.out.println("Число должно быть больше 0.\n" +
-                        "Попробуйте еще раз!\n");
-                return false;
-            }
-            return true;
-        }
-
-        static int[] generationArray(final int MIN, final int MAX, int n){
-            int[] array = new int[n];
-            for (int i = 0; i < array.length; i++){
-                array[i] = (int) (Math.random() * (MAX - MIN) + MIN);
-            }
-            return array;
-        }
-
-        int min(int[] array){
-            int min = array[0];
-            for (int i = 1; i < array.length; i++){
-                if (min > array[i]){
-                    min = array[i];
-                }
-            }
-            return min;
-        }
-
-        void maxIndex(int[] array){
-            for (int i = 1; i < array.length; i++){
-                if (array[indMax] <=array[i]) {
-                    indMax = i;
-                }
-            }
-        }
-
-        int getMaxInd(){
-            return indMax;
-        }
+    int getMaxInd() {
+        return indMax;
     }
+}
